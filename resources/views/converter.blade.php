1d0d4ecc5465058ef989de9bc5ba6515dfabbeab
Paweł Derehajło <derehajlo@gmail.com>
2016-05-06 15:53:48 +0200

    basic blade template with assets

---
+++ resources/views/converter.blade.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Author: Paweł Derehajło
+ * Contact: derehajlo@gmail.com
+ * Date: 06/05/16
+ */
+
+?>
+        <!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link type="text/css" href="{{ URL::asset('css/app.css') }}"/>
+    <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+
+    </div>
+</div>
+</body>
+</html>
+
