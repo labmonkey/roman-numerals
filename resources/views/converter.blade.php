425a790836e92efd529d4fba25766aa92abba761
Paweł Derehajło <derehajlo@gmail.com>
2016-05-07 20:34:12 +0200

    added frontend template

--- resources/views/converter.blade.php
+++ resources/views/converter.blade.php
@@ -1,53 +1,59 @@
-<?php
-
-/**
- * Author: Paweł Derehajło
- * Contact: derehajlo@gmail.com
- * Date: 06/05/16
- */
-
-?>
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel</title>
+    <title>Roman numerals converter</title>
 
-    <link type="text/css" href="{{ URL::asset('css/app.css') }}"/>
+    <link href='https://fonts.googleapis.com/css?family=Lato:100,400,300,700,900' rel='stylesheet' type='text/css'>
+
+    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/app.css') }}"/>
     <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
 </head>
 <body>
 <div class="container">
-    <div class="content">
-        {!! Form::open(array('url' => '/history')) !!}
-        {!! Form::text('number', isset($number) ? $number : '') !!}
-        {!! Form::submit('Submit') !!}
-        {!! Form::close() !!}
-        <span>@if (isset($roman)){!! $roman !!}@endif</span>
-        @if (count($history) > 0)
-            <ul>
-                @foreach ($history as $item)
-                    <li>{{ $item->number }} was converted to {{ $converter->toRomanNumerals($item->number) }}
-                        {{ \Carbon\Carbon::now()->diffInMinutes($item->created_at) }}
-                        minutes ago
-                    </li>
-                @endforeach
-            </ul>
-            @endif
-            @if (count($errors) > 0)
-                    <!-- Form Error List -->
+    <section class="header">
+        <h1>Roman numerals converter</h1>
+    </section>
+    <section class="errors">
+        @if (count($errors) > 0)
             <div class="alert alert-danger">
-                <strong>Whoops! Something went wrong!</strong>
-
-                <br><br>
-
                 <ul>
                     @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
+                        <li><span>Error:</span> {{ $error }}</li>
                     @endforeach
                 </ul>
             </div>
         @endif
-    </div>
+    </section>
+    <section class="form">
+        {!! Form::open(array('url' => '/history')) !!}
+        <div class="decimal">
+            {!! Form::text('number', isset($number) ? $number : '', array('class' => 'decimal-text')) !!}
+        </div>
+        <div class="submit">
+            {!! Form::button('Submit', array('class' => 'button-submit', "type" => "submit")) !!}
+        </div>
+        <div class="roman">
+            <span class="roman-text text">@if (isset($roman)){!! $roman !!}@else&nbsp;@endif</span>
+        </div>
+        {!! Form::close() !!}
+    </section>
+    <section class='list'>
+        <div class='history'>
+            @if (count($history) > 0)
+                <ul>
+                    @foreach ($history as $item)
+                        <li><span>{{ $item->number }}</span> was converted
+                            to <span>{{ $converter->toRomanNumerals($item->number) }}</span>
+                            <small>{{ \Carbon\Carbon::now()->diffInMinutes($item->created_at) }}
+                                minutes ago
+                            </small>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+
+    </section>
 </div>
 </body>
 </html>
